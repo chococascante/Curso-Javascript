f91d99dd98be9cd4c819b72e1db783e0fa239f19
Luis Cascante <luisalonzocascante@gmail.com>
2021-06-08 19:49:01 -0600

    receso

---
+++ Clases/Clase 9/index.js
@@ -0,0 +1,3 @@
+const { imprimirNombre, sumar2Numeros } = require("./utils");
+
+imprimirNombre("Hola Mundo, vengo de utils");
---
+++ Clases/Clase 9/utils.js
@@ -0,0 +1,12 @@
+function imprimirNombre(string) {
+  console.log(string);
+}
+
+function sumar2Numeros(num1, num2) {
+  return num1 + num2;
+}
+
+module.exports = {
+  imprimirNombre,
+  sumar2Numeros,
+};
